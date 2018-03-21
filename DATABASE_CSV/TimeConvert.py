12603c1f63a47958aca2455dc315be7fbd90c3d2
liuruogu <liuruogu1994@gmail.com>
2018-03-21 11:37:23 +0100

    Added the the dates input and only convert certain date

--- DATABASE_CSV/TimeConvert.py
+++ DATABASE_CSV/TimeConvert.py
@@ -4,6 +4,7 @@
 import os,sys
 import pandas as pd
 import glob
+import numpy as np
 import datetime
 
 CHUNKSIZE = 1000 # processing 100,000 rows at a time
@@ -24,47 +25,30 @@ def convert(inputData):
         time = converted_ticks.strftime("%Y-%m-%d %H:%M:%S")
         return time
 
-# def TickToTime(CsvChunk,ChunkIndex):
-#     """
-#     Definition: Go through all the current csv chunk and return the current chunk to be appended as one file
-# 	Parameters: CsvChunk: Current chunk, index [n,n+CHUNKSIZE];ChunkIndex:  starting index
-# 	Returns: CSV
-#     """
-#     # create a new dataframe to store df chunk
-#     NewCsv = pd.DataFrame(data=None, columns=CsvChunk.columns, index=CsvChunk.index)
-#
-#     timestamps = convert(CsvChunk["timestamp"][ChunkIndex])
-#
-#     # if timestamps[5:7] == '11':
-#     for i in range(len(CsvChunk)):
-#
-#         time = convert(CsvChunk["timestamp"][i+ChunkIndex])
-#
-#         # Store into a new dataframe
-#         NewCsv["timestamp"][i+ChunkIndex] = time
-#         NewCsv["value"][i+ChunkIndex] =  CsvChunk["value"][i+ChunkIndex]
-#         # CsvFile["timestamp"][i] = time
-#         # print(time)
-#
-#     print(NewCsv)
-#     print()
-#     # process data frame
-#     return NewCsv
-#     # else:
-#     #     return pd.DataFrame(columns=['timestamp','value'])
-
 def FindTheIndex(Data, StartDate, EndDate):
-
+    """
+    Definition: Go through the csv files and convert time format for each of the csv file
+	Parameters: FileName: All the csv file name on current directory
+	Returns: None
+    """
     Sindex = 0
     Eindex = 0
 
-    for each in Data:
-        timestamps = convert(Data["timestamp"][Sindex])
-        if timestamps[5:7] == '05':
-
+    for i in range(len(Data)):
+        timestamps = convert(Data["timestamp"][i])
+        # print(timestamps)
+        if timestamps[0:10] == StartDate:
+            Sindex = i
             break
 
+    for j in range(Sindex,len(Data)):
+        timestamps = convert(Data["timestamp"][j])
+        # print(timestamps)
+        if timestamps[0:10] == EndDate:
+            Eindex = j
+            break
 
+    print("The index of your date range: "+Sindex,Eindex)
     return Sindex, Eindex
 
 def ConvertFiles(FileName, StartDate, EndDate):
@@ -74,30 +58,31 @@ def ConvertFiles(FileName, StartDate, EndDate):
 	Returns: None
     """
     CsvFile = pd.read_csv(FileName,sep = ",")
-    NewCsv = pd.DataFrame(columns=CsvFile.columns,data=None)
 
     #Handle different type of Csv table structure, choose the tables have timestamp and value
     if 'timestamp'in CsvFile.columns and 'value' in CsvFile.columns:
-        # reader = pd.read_csv(FileName,sep = ",", chunksize=CHUNKSIZE)
-        # NewCsv = pd.DataFrame(columns=CsvFile.columns,data=None)
 
-        # chunkIndex = 0
-        # for df in reader:
-        #     currentIndex = CHUNKSIZE * chunkIndex
-        #     NewCsv = NewCsv.append(TickToTime(df,currentIndex))
-        #     chunkIndex = chunkIndex + 1
+        StartIndex,EndIndex = FindTheIndex(CsvFile, StartDate, EndDate)
 
-        # StartIndex
-        # EndIndex
+        if EndIndex == 0 :
+            sys.exit("The date doesnot exit or you have a wrong input ")
 
-        reader = pd.read_csv(FileName,sep = ",")
-        StartIndex,EndIndex = FindTheIndex(reader, StartDate, EndDate)
+        NewCsv = pd.DataFrame(columns=CsvFile.columns,data=None,index = np.arange(EndIndex-StartIndex))
+        NewCsvIndex = 0
 
-    #     NewCsv.to_csv("new_"+FileName,sep='\t', encoding='utf-8',index=False)
-    #
-    # else:
-    #     print(CsvFile.columns)
-    #     CsvFile.to_csv("new_"+FileName,sep='\t', encoding='utf-8',index=False)
+        for i in range(StartIndex, EndIndex):
+            time =convert(CsvFile["timestamp"][i])
+            # Store into a new dataframe
+            NewCsv["timestamp"][NewCsvIndex] = time
+            NewCsv["value"][NewCsvIndex] =  CsvFile["value"][i]
+            NewCsvIndex+=1
+
+        print(NewCsv)
+        NewCsv.to_csv("test_"+FileName,sep='\t', encoding='utf-8',index=False)
+
+    else:
+        print(CsvFile.columns)
+        CsvFile.to_csv("test_"+FileName,sep='\t', encoding='utf-8',index=False)
 
 def FileToConvert(SelectedFiles):
     """
